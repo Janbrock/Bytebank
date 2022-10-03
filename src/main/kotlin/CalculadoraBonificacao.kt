1a9ab6002ed3190e3d14912033e592dbede9cc0d
Janbrock <eujeanlima386@gmail.com>
2022-10-03 11:44:04 -0300

    Uso de polimorfismo pra cálculo de gastos totais

--- src/main/kotlin/CalculadoraBonificacao.kt
+++ src/main/kotlin/CalculadoraBonificacao.kt
@@ -1,17 +1,9 @@
 class CalculadoraBonificacao {
 
-    var total:Double = 0.0
+    var total: Double = 0.0
         private set
 
-    fun registra(funcionario: Funcionario){
+    fun registra(funcionario: Funcionario) {
         this.total += funcionario.bonificacao
     }
-    fun registra(gerente: Gerente){
-        this.total += gerente.bonificacao
-    }
-    fun registra(diretor: Diretor){
-        this.total += diretor.bonificacao
-    }
-
-
 }

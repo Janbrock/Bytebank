b6dd200c62607e1e875e84ed37ae8c763a742ddc
Janbrock <eujeanlima386@gmail.com>
2022-10-03 10:49:10 -0300

    Adição de gerente sem herança

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,13 +4,29 @@ fun main() {
     val alex = Funcionario(
         nome = "Alex",
         cpf = "111.111.111-11",
-        salario = 1000.0,
-        tipo = 0
+        salario = 1000.0
     )
     println("nome: ${alex.nome}")
     println("cpf: ${alex.cpf}")
     println("salário: ${alex.salario}")
     println("bonificação: ${alex.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+    println("nome: ${fran.nome}")
+    println("cpf: ${fran.cpf}")
+    println("salário: ${fran.salario}")
+    println("bonificação: ${fran.bonificacao()}")
+
+    if (fran.autentica(senha = 1234)){
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
 }
 
 

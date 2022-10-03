ff785b475d1a9218ce5c7f6229125f5f24e6845d
Janbrock <eujeanlima386@gmail.com>
2022-10-03 12:39:32 -0300

    Usando abstração para se dirigir aos funcionários

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,8 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
-
-    val contaCorrente = ContaCorrente(
-        "Alex",
-        1000
-    )
-    val contaPoupanca = ContaPoupanca(
-        "Fran",
-        1001
-    )
-
-    contaPoupanca.deposita(1000.0)
-    contaCorrente.deposita(1000.0)
-    println("saldo após depósito na Poupança: ${contaPoupanca.saldo}")
-    println("saldo após depósito na Corrente: ${contaCorrente.saldo}")
-
-    contaPoupanca.saca(100.0)
-    contaCorrente.saca(100.0)
-    println("saldo após saque na Poupança: ${contaPoupanca.saldo}")
-    println("saldo após saque na Corrente: ${contaCorrente.saldo}")
-
-    contaPoupanca.transfere(100.0, contaCorrente)
-    println("saldo após transferência na Poupança: ${contaPoupanca.saldo}")
-    contaCorrente.transfere(50.0, contaPoupanca)
-    println("saldo após transferência na Corrente: ${contaCorrente.saldo}")
-
+    testaFuncionarios()
 }
+
 
 
 
